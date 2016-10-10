c61f2f53f6521a7cd6548fbf38be2e9898ff272b
1nt3gr4 <vadem.gati@gmail.com>
2016-10-10 23:26:35 +0300

    Add files via upload

---
+++ range.19.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Вычисление pi*pi/18 по формуле суммы от m = 1 до бесконечности, где a = [(m-1)!]2 / (2m!).
+namespace range._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double eps = 0.000000001;
+            double m = 2, a = 0.5, sum = 0;
+
+            while (a > eps)
+            {
+                sum += a;
+                a *= (m - 1) * (m - 1) / (m + 1) / (m + 2);
+                m++;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(Math.PI * Math.PI / 18);
+        }
+    }
+}
