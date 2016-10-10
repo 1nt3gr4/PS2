3049343a5a457d18ca81b23370ccbb17358331d1
1nt3gr4 <vadem.gati@gmail.com>
2016-10-10 13:12:37 +0300

    Add files via upload

---
+++ letuchka 10.10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = 0;
+
+            for(int i = 40; i > 0; i--)
+                y = Math.Cos(y + i);
+
+            Console.WriteLine(y);
+        }
+    }
+}
