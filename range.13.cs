0efb4bef72a6b3f9fe08cd96e506a94a14e2342a
1nt3gr4 <vadem.gati@gmail.com>
2016-10-11 20:48:51 +0300

    Add files via upload

---
+++ range.13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace range._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10000;
+            double x;
+            Double.TryParse(Console.ReadLine(), out x);
+            double a = 1;
+            double sum = a;
+
+            for (int k = 0; k  < n; k ++)
+            {
+                a *= (-1) * x * (2 * k + 1) / (2 * k);
+                sum += a;
+            }
+
+            Console.WriteLine($"С помощью функции: {1.0 / Math.Sqrt(1 + x)}");
+            Console.WriteLine($"С помощью формулы: {sum}");
+        }
+    }
+}
